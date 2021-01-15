4fd224908ca3a9c3e97fcecd459830a5e6f58967
Marcus Camargo <mvocamargo@gmail.com>
2021-01-15 14:11:28 -0300

    Chatpter 4 - Core modules

---
+++ chapter-4/core.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const util = require('util');
+const v8 = require('v8');
+
+util.log(path.basename(__filename));
+util.log(v8.getHeapCodeStatistics());
+
+// const dirUploads = path.join(__dirname, 'www', 'files', 'uploads');
+// console.log(dirUploads);
